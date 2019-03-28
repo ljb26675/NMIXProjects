e2a5be2edbeee7ca2d8f90e0a81c15de493155ba
Linsey Briley <linsey@Linseys-Mac-mini.local>
2019-03-27 23:46:32 -0400

    added more info menu but stack views not resizing correctly on different screens

--- Projects/Midterm/Midterm/TableViewController2.swift
+++ Projects/Midterm/Midterm/TableViewController2.swift
@@ -1,5 +1,5 @@
 //
-//  TableViewController.swift
+//  TableViewController2.swift
 //  Midterm
 //
 //  Created by Linsey Briley on 3/27/19.
@@ -8,19 +8,19 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
-    
+class TableViewController2: UITableViewController {
+
     var dataValues: NSArray = []
     var candidates = 0
-
+    
     override func viewDidLoad() {
         self.tableView.rowHeight = 110
         loadData()
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
@@ -40,19 +40,19 @@ class TableViewController: UITableViewController {
         
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return dataValues.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Henry", for: indexPath)
         // theImage = "1s.jpg"
@@ -74,54 +74,55 @@ class TableViewController: UITableViewController {
         
         return cell
     }
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showStuff"{
             let destinationViewController = segue.destination as! ViewController
             let myIndexPath = self.tableView.indexPathForSelectedRow
             let myRow = myIndexPath!.row
             /*destinationViewController.myText = textForNewVieW[myRow]
-            destinationViewController.myLabel = myDataForTable[myRow]
-            destinationViewController.myImage = bigPixForView[myRow]*/
+             destinationViewController.myLabel = myDataForTable[myRow]
+             destinationViewController.myImage = bigPixForView[myRow]*/
             
             destinationViewController.curRow = myRow
             
         }
     }
-
+    
 }
+
--- Projects/Midterm/Midterm/ViewController.swift
+++ Projects/Midterm/Midterm/ViewController.swift
@@ -19,18 +19,29 @@ class ViewController: UIViewController {
     var curRow = 0
 
     @IBOutlet var catName: UILabel!
+   // @IBOutlet var temperament: UILabel!
+    //@IBOutlet var origin: UILabel!
     //@IBOutlet var catAttit: UILabel!
     
     @IBOutlet var catDes: UITextView!
     @IBOutlet var catAttit: UITextView!
-    @IBOutlet var catOrig: UITextView!
+    //@IBOutlet var catOrig: UITextView!
     
     @IBOutlet var picView: WKWebView!
     @IBOutlet var picText: UITextView!
     //@IBOutlet var picView: WKWebView!
     
+    
+    //@IBOutlet var moreInfo: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //catName.adjustsFontSizeToFitWidth = true
+        //desc.adjustsFontSizeToFitWidth = true
+        //temperament.adjustsFontSizeToFitWidth = true
+        //origin.adjustsFontSizeToFitWidth = true
+        
+        
+        
         loadData(whichCandidate: curRow)
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -53,7 +64,10 @@ class ViewController: UIViewController {
         loadData(whichCandidate: curRow)
     }
     
+    
     func loadData(whichCandidate: Int){
+        
+        
         let dataUrl = NSURL(string: "https://api.thecatapi.com/v1/breeds?api_key=9925340a-d1d0-48ce-88d5-31bda78cb13e")
         let imag1 = "https://www.google.com/search?q="
         //scottishfold
@@ -69,7 +83,9 @@ class ViewController: UIViewController {
         catName.text = (dataLine as AnyObject)["name"] as? String
         catDes.text = (dataLine as AnyObject)["description"] as? String
         catAttit.text = (dataLine as AnyObject)["temperament"] as? String
-        catOrig.text = (dataLine as AnyObject)["origin"] as? String
+        //moreInfo.text = "Want to know more about the " + catName.text! + "? Press the info button below!"
+        
+        //catOrig.text = (dataLine as AnyObject)["origin"] as? String
         let breedId = (dataLine as AnyObject)["id"] as? String
         
         let url1 = "https://api.thecatapi.com/v1/images/search?breed_id=" + breedId!
@@ -102,6 +118,22 @@ class ViewController: UIViewController {
         //label.text = (dataLine as AnyObject)["wikipedia_url"] as? String
         
         
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "info"{
+            let destinationViewController = segue.destination as! ViewController2
+            //let myIndexPath = self.tableView.indexPathForSelectedRow
+            //let myRow = myIndexPath!.row
+            /*destinationViewController.myText = textForNewVieW[myRow]
+             destinationViewController.myLabel = myDataForTable[myRow]
+             destinationViewController.myImage = bigPixForView[myRow]*/
+            
+            destinationViewController.dataValues = dataValues
+                destinationViewController.curRow = curRow
+            
+        }
     }
 
 
