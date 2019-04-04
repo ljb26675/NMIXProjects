c3b368152446c245c7998961a2bb31fcd4237857
Linsey Briley <linseybriley@NMI-Lab-2.local>
2019-04-04 10:42:40 -0400

    adding search using https://www.ioscreator.com/tutorials/add-search-table-view-ios-tutorial

---
+++ Projects/Midterm copy with search/Midterm/ToolbarController.swift
@@ -0,0 +1,45 @@
+//
+//  ToolbarController.swift
+//  Midterm
+//
+//  Created by Linsey Briley on 3/28/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ToolbarController: UIToolbar {
+
+    //var barStyle: UIBarStyle
+    //The toolbar style that specifies its appearance.
+    
+    //var barTintColor: UIColor?
+    //The tint color to apply to the toolbar background.
+    
+    //var tintColor: UIColor!
+    //The tint color to apply to the bar button items.
+    
+    //var isTranslucent: Bool
+    //A Boolean value that indicates whether the toolbar is translucent (true) or not (false).
+    
+    func backgroundImage(forToolbarPosition: UIBarPosition, barMetrics: UIBarMetrics) -> UIImage?{
+        
+    }
+    //Returns the image to use for the background in a given position and with given metrics.
+    
+    func setBackgroundImage(UIImage?, forToolbarPosition: UIBarPosition, barMetrics: UIBarMetrics){
+        
+    }
+    //Sets the image to use for the background in a given position and with given metrics.
+    
+    func shadowImage(forToolbarPosition: UIBarPosition) -> UIImage?{
+        
+    }
+    //Returns the image to use for the toolbar shadow in a given position.
+    
+    func setShadowImage(UIImage?, forToolbarPosition: UIBarPosition){
+        
+    }
+    //Sets the image to use for the toolbar shadow in a given position.
+
+}
